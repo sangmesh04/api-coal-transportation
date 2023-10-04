1f6da992baf040968c3cffc365b0e6e104596cc5
sangmesh04 <78370367+sangmesh04@users.noreply.github.com>
2023-10-04 16:03:02 +0530

    updated initial get request

--- routes/route.js
+++ routes/route.js
@@ -2,8 +2,13 @@ const express = require("express");
 const shortestPath = require("../controllers/shortestPath");
 const router = new express.Router();
 
-router.get("/", () => {
-  return console.log("sangmeshwar mahajan");
+router.get("/", (req, res) => {
+  try {
+    return res.status(200).json({ data: "hello world!", status: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: err.message, status: false });
+  }
 });
 
 router.post("/findRoute", shortestPath.shortestRoute);
